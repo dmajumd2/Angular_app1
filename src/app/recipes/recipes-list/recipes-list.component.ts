d900dec9402f8cd3949261400ca58520df753b7f
Debashish <dmajumd2@binghamton.edu>
2018-02-02 17:56:26 -0500

    Add: Added a recipe model for displaying the recipe list

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-
+recipes: Recipe[] = [
+	new Recipe('Test Recipe', 'Test description', 'http://img.taste.com.au/QOHMXuXS/w720-h480-cfill-q80/taste/2016/11/chicken-cacciatore-tray-bake-83394-1.jpeg');
+	
+	new Recipe('Test Recipe', 'Test description', 'https://cdn-maf0.heartyhosting.com/sites/muscleandfitness.com/files/lean-crispy-chicken-breast-recipe.jpg');
+];
+	
   constructor() { }
 
   ngOnInit() {
