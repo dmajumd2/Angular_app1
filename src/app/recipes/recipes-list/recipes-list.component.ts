978369be2ae5928a9e50586239557788756caed1
Debashish <dmajumd2@binghamton.edu>
2018-02-03 20:49:08 -0500

    Added a code in recipe detail component for displaying the recipe detail

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -6,11 +6,16 @@ import { Recipe } from '../recipe.model';
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
+
 export class RecipesListComponent implements OnInit {
-recipes: Recipe[] = [
-	new Recipe('Test Recipe', 'Test description', 'http://img.taste.com.au/QOHMXuXS/w720-h480-cfill-q80/taste/2016/11/chicken-cacciatore-tray-bake-83394-1.jpeg');
 	
-	new Recipe('Test Recipe', 'Test description', 'https://cdn-maf0.heartyhosting.com/sites/muscleandfitness.com/files/lean-crispy-chicken-breast-recipe.jpg');
+recipes: Recipe[] = [
+	new Recipe('Test Recipe', 'Test description', 'http://img.taste.com.au/QOHMXuXS/w720-h480-cfill-q80/taste/2016/11/chicken-cacciatore-tray-bake-83394-1.jpeg'),
+	
+	new Recipe('Test Recipe', 'Test description', 'http://img.taste.com.au/QOHMXuXS/w720-h480-cfill-q80/taste/2016/11/chicken-cacciatore-tray-bake-83394-1.jpeg'),
+	
+	new Recipe('Test Recipe', 'Test description', 'http://img.taste.com.au/QOHMXuXS/w720-h480-cfill-q80/taste/2016/11/chicken-cacciatore-tray-bake-83394-1.jpeg')
+		
 ];
 	
   constructor() { }
