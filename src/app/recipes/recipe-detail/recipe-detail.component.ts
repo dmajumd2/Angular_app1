0d08068cf583b48de4e0f30382fad1d4c30324a5
Debashish <dmajumd2@binghamton.edu>
2018-02-18 11:17:24 -0500

    Added roting for edit button

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RecipeService } from '../recipe.service';
-import { ActivatedRoute, Params } from '@angular/router';
-
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -14,7 +13,8 @@ recipe: Recipe;
 	id: number;
 	
   constructor(private recipeService: RecipeService,
-			  private route: ActivatedRoute) { }
+			  private route: ActivatedRoute,
+			  private router: Router) { }
 
   ngOnInit() {
 	  this.route.params
@@ -27,5 +27,9 @@ recipe: Recipe;
 
 	onAddToShoppingList(){
 		this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+	}
+	
+	onEditRecipe(){
+		this.router.navigate(['edit'], {relativeTo: this.route});
 	}
 }
