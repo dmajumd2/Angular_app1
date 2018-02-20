43d5ce33944d83ee1958c93c208904e8d1936c25
Debashish <dmajumd2@binghamton.edu>
2018-02-20 11:29:57 -0500

    Added observables

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,11 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
-	recipeSelected = new EventEmitter<Recipe>();
+	
 	
 	private recipes: Recipe[] = [
 	new Recipe('Chicken', 'A Super tasty', 'http://img.taste.com.au/QOHMXuXS/w720-h480-cfill-q80/taste/2016/11/chicken-cacciatore-tray-bake-83394-1.jpeg', [
